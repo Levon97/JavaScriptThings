d88d0c4ffe8194b84b2d8f9e2b0e04c664b455e5
Levon97 <levon.samsonyan@gmail.com>
2021-03-28 01:47:34 +0400

    just testing  commiut with new PC

--- repositoryCach/src/configs/db.js
+++ repositoryCach/src/configs/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   url: 'mongodb+srv://levon:dbUserFeanor@projectx.0vbdk.mongodb.net/reposDb?retryWrites=true&w=majority',
   options: {useNewUrlParser: true, useUnifiedTopology: true}
-};
+}; 
+
+// this is comment
